1d2a76641f6d97afa13e1b1c68c0ad588ebb98bc
pixelzen557 <yooha.benji@gmail.com>
2026-03-10 09:45:36 -0400

    updated band_name_generator.py
    updated band_name_generator.py to complete my first python project.

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,2 +1,4 @@
-print("Welcome to Band Name Generator")
-print("What's the name of the city you grew up in?")
+print("Welcome to the Bank Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be", city_name, pet_name)
